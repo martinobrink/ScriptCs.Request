62e63e89150f9061fbf970bfc208b7210cf7cca1
Martin Öbrink-Hansen <martin@obrink-hansen.dk>
2014-01-26 23:28:37 +0100

    initial commit after moving from ScriptCs.EasyHttp to ScriptCs.Request package name

--- README.md
+++ README.md
@@ -1,4 +1,19 @@
 ScriptCs.Request
-================
+=================
+A ScriptCs script pack which allows you to write clean, declarative and non-boilerplate http requests from your scriptcs (.csx) files.
 
-This is a ScriptCs script pack which allows you to write clean, declarative and non-boilerplate http requests from your scriptcs files.
+Using this script pack you can write very simple scripts similar to this:
+
+
+	public class Notification
+	{
+		public string Message {get; set;}
+		public string SenderName {get; set;}
+	}
+	var notificationToSend = new Notification { Message = "Merry christmas!", SenderName = "Santa"};
+	var client = Require<Request>();
+	var result = client.PostJson("http://your.site.com/api/notification", notificationToSend);
+	Console.WriteLine("Response status code: " + result.StatusCode);
+
+
+The script pack also has the Json.NET namespace available so you can make use of different Json.NET serialization possibilities (e.g. [JsonIgnore]) to suit your needs. 
---
+++ ScriptCs.Request/Request.cs
@@ -0,0 +1,47 @@
+﻿using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using Newtonsoft.Json;
+using ScriptCs.Contracts;
+
+namespace ScriptCs.Request
+{
+    public class Request : IScriptPackContext
+    {
+        public string Get(string url)
+        {
+            var httpClient = new HttpClient();
+            return httpClient.GetStringAsync(url).Result;
+        }
+
+        public HttpResponseMessage GetJson(string url)
+        {
+            var httpClient = new HttpClient();
+            httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            return httpClient.GetAsync(url).Result;
+        }
+
+        public HttpResponseMessage PostJson<T>(string url, T objectToPost) where T : class
+        {
+            var httpClient = new HttpClient();
+            var bodyContent = new StringContent(JsonConvert.SerializeObject(objectToPost), Encoding.UTF8, "application/json");
+            httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            return httpClient.PostAsync(url, bodyContent).Result;
+        }
+
+        public HttpResponseMessage PutJson<T>(string url, T objectToPut) where T : class
+        {
+            var httpClient = new HttpClient();
+            var bodyContent = new StringContent(JsonConvert.SerializeObject(objectToPut), Encoding.UTF8, "application/json");
+            httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            return httpClient.PutAsync(url, bodyContent).Result;
+        }
+
+        public HttpResponseMessage Delete(string url)
+        {
+            var httpClient = new HttpClient();
+            httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            return httpClient.DeleteAsync(url).Result;
+        }
+    }
+}
